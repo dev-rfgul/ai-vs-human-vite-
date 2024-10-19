fc5b7b0c57bca202bb33211623ac26f30f1c1abb
dev-rfgul <rfgul587@gmail.com>
2024-10-19 16:33:27 +0500

    added the yt video if the api doesnt work

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ export default function Page() {
   return (
     <>
 
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Zj_tFT49liU?si=Xhn0S97oBXaofWMe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
       {currentGame === "human" ? (
         <HumanGuess onGameEnd={changeGameMode} />
